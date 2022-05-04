31d36030d4f792fad6878d5e95783744b710896d
Nastassia Stanchyk <stanchikaf@gmail.com>
2022-05-04 14:27:40 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/house/Main.java
+++ src/main/java/com/epam/rd/autotasks/house/Main.java
@@ -14,16 +14,16 @@ public class Main {
         Cat barbos = new Cat("Barbos");
         Kitten murzik = new Kitten("Murzik");
 
-        House dogHouse = new House();
+        House<Dog> dogHouse = new House();
         dogHouse.enter(rex);
         dogHouse.enter(randy);
-        dogHouse.enter(murzik); //This must fail on compilation stage if you parameterize the dogHouse. Delete the line when solution is ready
+        //dogHouse.enter(murzik); //This must fail on compilation stage if you parameterize the dogHouse. Delete the line when solution is ready
         System.out.println(dogHouse);
 
-        House catHouse = new House();
+        House<Cat> catHouse = new House();
         catHouse.enter(barbos);
         catHouse.enter(murzik);
-        catHouse.enter(rex); //This must fail on compilation stage if you parameterize the catHouse. Delete the line when solution is ready
+        //catHouse.enter(rex); //This must fail on compilation stage if you parameterize the catHouse. Delete the line when solution is ready
         System.out.println(catHouse);
     }
 }
